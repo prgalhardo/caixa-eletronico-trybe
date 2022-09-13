3fd46e802f33af33b1440fabbe25b3831530cf85
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-13 20:42:20 -0300

    Implementação construtor PessoaCliente

--- src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
@@ -1,5 +1,22 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class PessoaCliente {
+import java.util.ArrayList;
 
+
+/**
+ * Classe: Pessoa Cliente.
+ */
+
+public class PessoaCliente {
+  String nomeCompleto;
+  String cpf;
+  String senha;
+  ArrayList<Conta> contas;
+
+  PessoaCliente(String nomeCompleto, String cpf, String senha) {
+    this.nomeCompleto = nomeCompleto;
+    this.cpf = cpf;
+    this.senha = senha;
+    System.out.println("Nova pessoa cliente " + nomeCompleto + " com CPF: " + cpf + " criada!");
+  }
 }
