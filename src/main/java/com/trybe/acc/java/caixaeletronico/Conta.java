80523e8a34ae9038613cd1262657da65903380a8
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-14 01:05:06 -0300

    Implementação dos métodos da classe Conta

--- src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
  */
 
 public class Conta {
+
   String tipoConta;
   String idConta;
   PessoaCliente pessoaCliente;
@@ -16,6 +17,58 @@ public class Conta {
     this.tipoConta = tipoConta;
     this.pessoaCliente = pessoaCliente;
     this.idConta = banco.gerarNumeroNovaConta();
+  }
+
+  public String getTipoConta() {
+    return tipoConta;
+  }
+
+  public String getIdConta() {
+    return idConta;
+  }
+
+  public PessoaCliente getPessoaCliente() {
+    return pessoaCliente;
+  }
+
+  public ArrayList<Transacao> getTransacoes() {
+    return transacoes;
+  }
+
+  /**
+   * Método: Adicionar Transação.
+   */
+  public void adicionarTransacao(double quantia, String descricao) {
+    Transacao transacao = new Transacao(quantia, descricao);
+    transacoes.add(transacao);
+  }
+
+  /**
+   * Método: Retornar Saldo.
+   */
+  public double retornarSaldo() {
+    double total = 0;
+    for (Transacao transacao : transacoes) {
+      total += transacoes.getQuantia();
+    }
+    return total;
+  }
+
+  /**
+   * Método: Retornar Resumo Conta.
+   */
+  public String retornarResumoConta() {
+    return "Número de identificação único: " + getIdConta() + ". Saldo: " + retornarSaldo() +
+            ". Tipo de Conta: " + getTipoConta() + ".";
+  }
+
+  /**
+   * Método: Retornar Extrato.
+   */
+  public void retornarExtrato() {
+    for (Transacao transacao : transacoes) {
+      System.out.println(transacao.retornarResumoTransacao);
+    }
   }
 
 }
