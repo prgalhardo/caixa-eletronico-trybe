a869d5283ba9423b5307a9d2c16405ba7e7ef8b7
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-13 22:38:01 -0300

    Implementação da classe PessoaCliente com seus métodos.

--- src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
@@ -1,6 +1,7 @@
 package com.trybe.acc.java.caixaeletronico;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 /**
@@ -18,5 +19,70 @@ public class PessoaCliente {
     this.cpf = cpf;
     this.senha = senha;
     System.out.println("Nova pessoa cliente " + nomeCompleto + " com CPF: " + cpf + " criada!");
+  }
+
+  /**
+   * Método: Adicionar conta.
+   */
+  public void adicionarConta(Conta conta) {
+    contas.add(conta);
+  }
+
+  /**
+   * Método: Retornar número de contas.
+   */
+  public int retornaNumeroDeContas() {
+    return contas.size();
+  }
+
+  /**
+   * Método: Retornar Saldo Conta Específica.
+   */
+  public double retornarSaldoContaEspecifica(int indice) {
+    Conta conta = contas.get(indice);
+    return conta.retornarSaldo();
+  }
+
+  /**
+   * Método: Retornar Id Conta Específica.
+   */
+  public String retornarIdContaEspecifica(int indice) {
+    Conta conta = contas.get(indice);
+    return conta.getIdConta();
+  }
+
+  /**
+   * Método: Retornar Extrato Conta Específica.
+   */
+  public void retornarExtratoContaEspecifica(int indice) {
+    Conta conta = contas.get(indice);
+    conta.retornarExtrato();
+  }
+
+  /**
+   * Método: adicionarTransacaoContaEspecifica.
+   */
+  public void adicionarTransacaoContaEspecifica(int indice, double quantia, String descricao) {
+    Conta conta = contas.get(indice);
+    conta.adicionarTransacao(quantia, descricao);
+  }
+
+  /**
+   * Método: validarSenha.
+   */
+  public boolean validarSenha(String senha) {
+    return Objects.equals(this.senha, senha);
+  }
+
+  /**
+   * Método: retornarResumoContas.
+   */
+  public void retornarResumoContas() {
+    for (Conta conta : contas) {
+      System.out.println(conta.retornarResumoConta());
+    }
+  }
+  public String getCpf() {
+    return cpf;
   }
 }
