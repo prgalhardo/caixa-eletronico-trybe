d90a5ecc43d677616524c4d2106fa5632062b0f1
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-14 04:33:01 -0300

    TransacaoTest - método getter de quantia e correção método construtor

--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -14,7 +14,7 @@ class TransacaoTest {
   void construtorTest() {
     Transacao transacao = new Transacao(500.00, "Transação realizada.");
 
-    assertEquals(500.00, transacao.getQuantia());
+    assertEquals(500.00, transacao.quantia);
     assertEquals("Transação realizada.", transacao.descricao);
   }
 
@@ -22,7 +22,9 @@ class TransacaoTest {
   @Test
   @DisplayName("22 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
+    Transacao transacao = new Transacao(500.00, "Transação realizada.");
+
+    assertEquals(500.00, transacao.getQuantia());
 
   }
 
