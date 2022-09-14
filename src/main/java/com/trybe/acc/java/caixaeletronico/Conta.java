79f62a81edf7a8c065735b003ae2c1fe56f76520
Priscila Silva <priscilacds_100@yahoo.com.br>
2022-09-13 23:26:54 -0300

    Implementação construtor da classe Conta

--- src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
@@ -1,6 +1,21 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class Conta {
+import java.util.ArrayList;
 
+/**
+ * Classe: Conta.
+ */
+
+public class Conta {
+  String tipoConta;
+  String idConta;
+  PessoaCliente pessoaCliente;
+  ArrayList<Transacao> transacoes;
+
+  Conta(String tipoConta, PessoaCliente pessoaCliente, Banco banco) {
+    this.tipoConta = tipoConta;
+    this.pessoaCliente = pessoaCliente;
+    this.idConta = banco.gerarNumeroNovaConta();
+  }
 
 }
